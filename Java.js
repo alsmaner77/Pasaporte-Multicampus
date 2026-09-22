6408ffe790b49c16e1f1edb518afd539c4cab6fd
alsmaner77 <alsmaner77@gmail.com>
2026-09-21 19:21:16 -0500

    Update Java.js

--- Java.js
+++ Java.js
@@ -1,5 +1,5 @@
 // Manejo de pestañas de navegación
-function openTab(evt, tabName) {
+windows.openTab(evt, tabName) {
     const tabContents = document.getElementsByClassName("tab-content");
     for (let i = 0; i < tabContents.length; i++) {
         tabContents[i].classList.remove("active");
@@ -21,7 +21,7 @@ let completedChallenges = 0;
 const mockCities = ["Campus Monterrey", "Campus Guadalajara", "Campus Puebla", "Campus Querétaro", "Campus Reynosa"];
 
 // Lógica para completar un reto
-function completeChallenge(challengeId, badgeName, progressIncrease) {
+windows.completeChallenge(challengeId, badgeName, progressIncrease) {
     // 1. Actualizar botón del reto
     const btn = document.querySelector(`#reto-${challengeId} button`);
     btn.innerText = "Reto Completado ✅";
