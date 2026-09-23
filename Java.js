9fcad929b02b001eb0cedc760d70049581a45d98
alsmaner77 <alsmaner77@gmail.com>
2026-09-22 22:23:39 -0500

    Update Java.js

--- Java.js
+++ Java.js
@@ -283,7 +283,13 @@ window.completeChallenge = async function(challengeId, badgeName, progressIncrea
     const userRef = doc(db, "usuarios", userId);
 
     try {
+        // Calculamos el incremento normal
         let nuevoProgreso = progress + progressIncrease;
+       
+        if (completedChallenges == 6 || nuevoProgreso >= 99) {
+            nuevoProgreso = 100;
+        }
+
         if (nuevoProgreso > 100) nuevoProgreso = 100;
 
         await updateDoc(userRef, {
